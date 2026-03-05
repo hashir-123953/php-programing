3e06ced5ac1bab0a9135492daacb5c2d4426657e
Hashir Nadeem <mhashirnadeem464@gmail.com>
2026-03-05 20:21:04 +0500

    datatype assgnment

---
+++ Datatype.php
@@ -0,0 +1,58 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    //student //
+    $student_name="Hashir nadeem";
+    $student_age=26;
+    $student_height=5.2;
+    $is_active=TRUE;
+    $student_friends=array("Hammad","Jazil","Fakir","Aysha");
+
+    echo "<h1> Students Details:</h1><br>";
+    echo"Student name: $student_name<br>";
+    echo"Student age: $student_age<br>";
+    echo"Student height: $student_height<br>";
+    echo"Student status: $is_active<br>";
+    echo" $student_friends[0]<br>";
+    echo" $student_friends[1]<br>";
+    echo" $student_friends[2]<br>";
+    echo" $student_friends[3] <br>";
+
+
+    
+    
+
+
+// employ
+
+        $employ_id=100;
+        $employ_name="Shahzaib";
+        $employ_height=6.3;
+        $married_status=false;
+        $employ_do_jobs=array("Infotech","Codetech","Codinglover");
+
+     echo "<h1> Employ Details:</h1><br>";
+     echo"Employ name: $employ_id<br>";
+    echo" Employage: $employ_name<br>";
+    echo"Employ height: $employ_height<br>";
+    echo"Married_status: $married_status<br>";
+    echo" $employ_do_jobs[0]<br>";
+    echo" $employ_do_jobs[1]<br>";
+    echo" $employ_do_jobs[2]";
+   
+
+
+
+    
+    ?>
+</body>
+</html>
